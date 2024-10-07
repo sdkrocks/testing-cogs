b6df97050ea7598bd119ba29ecd295edc25c368d
sdkrocks <kernerslayde@gmail.com>
2024-10-07 05:03:08 -0400

    Update __init__.py

--- Scrub/__init__.py
+++ Scrub/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .scrub import Scrub
 
-async def setup(bot: commands.Bot):
+async def setup(bot):
     """Setup function to add the Scrub cog."""
     await bot.add_cog(Scrub(bot))
