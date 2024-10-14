7c6024df4e1d47ee0703721e89af35679091c133
Slayde <kernerslayde@gmail.com>
2024-10-13 22:17:36 -0400

    Fix inits

--- Scrub/__init__.py
+++ Scrub/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from .vcdetect import vcdetect
+from .scrub import Scrub
 
 async def setup(bot):
-    """Setup function to add the vcdetect cog."""
-    await bot.add_cog(vcdetect(bot))
+    """Setup function to add the Scrub cog."""
+    await bot.add_cog(Scrub(bot))
