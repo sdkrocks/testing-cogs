d5b995530795be313aebdadc58659aefc04b9aac
Slayde <kernerslayde@gmail.com>
2024-10-13 22:18:52 -0400

    Fix import

--- voice-detect/__init__.py
+++ voice-detect/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from .vcdetect import vcdetect
+from .vcdetect import VoiceWordListener
 
 async def setup(bot):
     """Setup function to add the vcdetect cog."""
-    await bot.add_cog(vcdetect(bot))
+    await bot.add_cog(VoiceWordListener(bot))
